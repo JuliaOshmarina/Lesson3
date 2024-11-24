317a78effc4338d083ffd431ae4089af3245bf29
JuliaOshmarina <ju99478@gmail.com>
2024-11-24 23:36:58 +0500

    added build.gradle

--- src/test/java/tests/Lesson3.java
+++ src/test/java/tests/Lesson3.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class lesson3 {
+public class Lesson3 {
 
     @BeforeAll
     static void beforeAll() {
