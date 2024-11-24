63ad0e4af7211b6ed0a4bcdc767e0fa3c4b55078
JuliaOshmarina <ju99478@gmail.com>
2024-11-24 23:30:18 +0500

    added build.gradle

--- src/test/java/tests/lesson3.java
+++ src/test/java/tests/lesson3.java
@@ -4,7 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,7 +15,6 @@ public class lesson3 {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
@@ -22,11 +23,15 @@ public class lesson3 {
     @Test
     void successfulSearchTest() {
         open("https://demoqa.com/automation-practice-form");
+
+        //Datа
         $("[id=firstName]").setValue("Julia");
         $("[id=lastName]").setValue("Oshmarina");
         $("[id=userEmail]").setValue("juffgdfgd@gmail.com");
         $(byText("Male")).click();
         $("[id=userNumber]").setValue("4544263545");
+
+        //birth
         $("[id=dateOfBirthInput]").click();
         $("[class~=react-datepicker__year-select]").click();
         $(".react-datepicker__year-select").selectOption("2001");
@@ -35,11 +40,36 @@ public class lesson3 {
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__month-select").click();
         $("[class~=react-datepicker__day--018]").click();
+
+        //Data
         $("#subjectsInput").setValue("English").pressEnter();
         $(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("1.png");
+
+        //Picture
+        File cv = new File("src/resources/test.jpg");
+        $("#uploadPicture").uploadFile(cv);
 
 
+        //Adress
+        $("[id=currentAddress]").setValue("Russia");
+        $("[class~=css-1wa3eu0-placeholder]").click();
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaiselmer").pressEnter();
+
+        $("#submit").click();
+
+
+        //Examination
+        $(".table").$(byText("Student Name")).sibling(0).shouldHave(text("Julia Oshmarina"));
+        $(".table").$(byText("Student Email")).sibling(0).shouldHave(text("juffgdfgd@gmail.com"));
+        $(".table").$(byText("Gender")).sibling(0).shouldHave(text("Male"));
+        $(".table").$(byText("Mobile")).sibling(0).shouldHave(text("4544263545"));
+        $(".table").$(byText("Date of Birth")).sibling(0).shouldHave(text("18 April,2001"));
+        $(".table").$(byText("Subjects")).sibling(0).shouldHave(text("English"));
+        $(".table").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports"));
+        $(".table").$(byText("Picture")).sibling(0).shouldHave(text("test.jpg"));
+        $(".table").$(byText("Address")).sibling(0).shouldHave(text("Russia"));
+        $(".table").$(byText("State and City")).sibling(0).shouldHave(text("Rajasthan Jaiselmer"));
 
     }
 }
