7e4d97babcda886fdea1fb90e2e38708692c065a
JuliaOshmarina <ju99478@gmail.com>
2025-01-17 00:10:32 +0500

    added Lesson11

--- src/test/java/tests/Lesson11.java
+++ src/test/java/tests/Lesson11.java
@@ -1,29 +1,58 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.File;
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class Lesson3 {
+public class Lesson11 {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @Test
+    @Tag("lesson")
     void successfulSearchTest() {
+        step("Открываем главную страницу", () -> {
         open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
+        });
 
+        step("Заполняем поля", () -> {
         //Datа
         $("[id=firstName]").setValue("Julia");
         $("[id=lastName]").setValue("Oshmarina");
@@ -50,7 +79,6 @@ public class Lesson3 {
         //Picture
         $("#uploadPicture").uploadFromClasspath("test.jpg");
 
-
         //Adress
         $("[id=currentAddress]").setValue("Russia");
         $("[class~=css-1wa3eu0-placeholder]").click();
@@ -58,9 +86,9 @@ public class Lesson3 {
         $("#react-select-4-input").setValue("Jaiselmer").pressEnter();
 
         $("#submit").click();
+        });
 
-
-        //Examination
+        step("Проверяем заполнение полей", () -> {
         $(".table").$(byText("Student Name")).sibling(0).shouldHave(text("Julia Oshmarina"));
         $(".table").$(byText("Student Email")).sibling(0).shouldHave(text("juffgdfgd@gmail.com"));
         $(".table").$(byText("Gender")).sibling(0).shouldHave(text("Male"));
@@ -71,6 +99,6 @@ public class Lesson3 {
         $(".table").$(byText("Picture")).sibling(0).shouldHave(text("test.jpg"));
         $(".table").$(byText("Address")).sibling(0).shouldHave(text("Russia"));
         $(".table").$(byText("State and City")).sibling(0).shouldHave(text("Rajasthan Jaiselmer"));
-
+        });
     }
 }
